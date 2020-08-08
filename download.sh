13778717f9d3572cbce622278621c2e816836433
Kairong Jiang <jiangkairong@email.arizona.edu>
2020-08-08 02:39:18 -0700

    modified download

--- download.sh
+++ download.sh
@@ -1,11 +1,7 @@
 
 # Download Hotpot Data
-wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_distractor_v1.json
-wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_fullwiki_v1.json
 wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.1.json
 
-mkdir hotpot
+mkdir data
 
-mv hotpot_dev_distractor_v1.json hotpot/
-mv hotpot_dev_fullwiki_v1.json hotpot/
-mv hotpot_train_v1.1.json hotpot/
+mv hotpot_train_v1.1.json data/
